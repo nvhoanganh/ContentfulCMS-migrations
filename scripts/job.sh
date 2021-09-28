c4f999adff6680b6c40c72b3074029ac61dc6a23
Anthony Nguyen <nvhoanganh1909@gmail.com>
2021-09-28 13:00:25 +1000

    job

--- scripts/job.sh
+++ scripts/job.sh
@@ -1,13 +1,27 @@
-# download
+echo "Creating new branch $1"
+
+# create branch if not exists
+git branch $1 || true
+
+# check out to that branch
+git checkout $1
+
+# download json for the content
 export ENV=$FROMENV
 export SPACEID=$FROMSPACEID
 export ACCESSTOKEN=$FROMACCESSTOKEN
 
 sh scripts/download.sh $1
 
-# uppload
-export ENV=$TOENV
-export SPACEID=$TOSPACEID
-export ACCESSTOKEN=$TOACCESSTOKEN
+# commit
+echo "Commit changes"
+git commit -am "Content ID $1 updated"
 
-sh scripts/import.sh $1
+# create a PR
+
+# # uppload
+# export ENV=$TOENV
+# export SPACEID=$TOSPACEID
+# export ACCESSTOKEN=$TOACCESSTOKEN
+
+# sh scripts/import.sh $1
