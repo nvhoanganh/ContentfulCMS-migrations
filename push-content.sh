470626692bd31e712ec5acc51030859c8fe73c24
Anthony Nguyen <nvhoanganh1909@gmail.com>
2021-09-28 17:40:04 +1000

    merge

--- push-content.sh
+++ push-content.sh
@@ -1,9 +1,10 @@
-echo " 🎉 Importing $1 into $SPACEID-$ENV" 
+fileChanges=$(git diff HEAD~1 --name-only | grep contents-migrations)
+echo " 🎉 Importing $fileChanges into $SPACEID-$ENV" 
 
 node_modules/.bin/contentful space import --space-id $SPACEID \
     --environment-id $ENV \
     --mt $ACCESSTOKEN \
-    --content-file ./contents-migrations/$1.json \
+    --content-file ./$fileChanges \
     --skip-content-model true \
     --error-log-file ./scripts/error.log
 
