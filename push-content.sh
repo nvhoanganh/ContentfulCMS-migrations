2d8e432f0ed8aeffee1976de0b6b500ed70e04cc
Anthony Nguyen <nvhoanganh1909@gmail.com>
2021-09-29 09:42:26 +1000

    debug

--- push-content.sh
+++ push-content.sh
@@ -1,10 +1,15 @@
-fileChanges=$(git diff HEAD~1 --name-only | grep contents-migrations)
-echo " 🎉 Importing $fileChanges into $SPACEID-$ENV" 
+fileChanges=$(git diff HEAD~0 --name-only | grep contents-migrations)
+if [ -z "$fileChanges" ]
+then
+    echo " ⭐️ no changes found"
+else
+    echo " 🎉 Importing $fileChanges into $SPACEID-$ENV"
 
-node_modules/.bin/contentful space import --space-id $SPACEID \
+    node_modules/.bin/contentful space import --space-id $SPACEID \
     --environment-id $ENV \
     --mt $ACCESSTOKEN \
     --content-file ./$fileChanges \
     --skip-content-model true \
     --error-log-file ./scripts/error.log
+fi
 
