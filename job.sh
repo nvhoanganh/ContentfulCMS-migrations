e079a94bfcb9cbf3c79347bfaf70c4193055ad7a
Anthony Nguyen <nvhoanganh1909@gmail.com>
2021-09-28 13:13:11 +1000

    update job

--- job.sh
+++ job.sh
@@ -9,6 +9,7 @@ git checkout $1
 # download json for the content
 export ENV=$FROMENV
 export SPACEID=$FROMSPACEID
+SOURCE_SPACEID=$FROMSPACEID
 export ACCESSTOKEN=$FROMACCESSTOKEN
 
 sh scripts/download.sh $1
@@ -18,9 +19,12 @@ echo "Add and Commit ./contents-migrations/$1.json file"
 git add ./contents-migrations/$1.json
 git commit -am "Content ID $1 changed"
 
-# create a PR
+# create a PR using Github action
+echo "Pushing $1 branch to remote"
+git push --set-upstream origin $1 || true
+gh pr create --base master --title "Content $1 updated" --body "This Content was updated https://app.contentful.com/spaces/$SOURCE_SPACEID/entries/$1"
 
-# # uppload
+# uppload
 # export ENV=$TOENV
 # export SPACEID=$TOSPACEID
 # export ACCESSTOKEN=$TOACCESSTOKEN
